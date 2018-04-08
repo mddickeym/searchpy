2ea8a4b3c10aaa3ca59551d75c0568837c885511
mddickeym <mddickeym@yahoo.com>
2018-04-08 07:33:33 -0500

    Update searchpy.py

--- searchpy.py
+++ searchpy.py
@@ -12,6 +12,7 @@ from tweepy.streaming import StreamListener
 from dash.dependencies import Input, Output
 
 
+
 # from wordcloud import WordCloud
 CONSUMER_KEY = "u2w4FopTMw3xYSwjUSzSvhfe1"
 CONSUMER_SECRET = "wtFwfU9qIviOSjey8KY4gLWl37x6uyUJTdU3aKvGimi42jVJQt"
@@ -26,9 +27,17 @@ api = tweepy.API(auth)
 analyzer = SentimentIntensityAnalyzer()
 
 query = 'tester'
+
+##raw query; courtesy http://python-twitter.readthedocs.io/en/latest/searching.html
+
+results = api.GetSearch(raw_query="q=?%20&result_type=recent&since=2018-04-8&count=100")
+
 max_tweets = 100
 public_tweets = [status for status in tweepy.Cursor(
     api.search, q=query, lang="en").items(max_tweets)]
+
+
+
 
 data = [[obj.user.screen_name, obj.user.name, obj.user.id_str, obj.user.description, obj.created_at.year, obj.created_at.month, obj.created_at.day,
          "%s.%s" % (obj.created_at.hour, obj.created_at.minute), obj.id_str, obj.text, analyzer.polarity_scores(obj.text)["compound"]] for obj in public_tweets]
@@ -47,6 +56,7 @@ neutral = 0
 negative = 0
 compound_scores = []
 
+
 for tweet in public_tweets:
 
     compound = analyzer.polarity_scores(tweet.text)["compound"]
@@ -64,6 +74,30 @@ compound_average = round(numpy.mean(compound_scores), 5)
 # Dash Stuff!
 app = dash.Dash()
 
+##CACHE_CONFIG = {
+#    'CACHE_TYPE': 'redis',
+#    'CACHE_REDIS_URL': os.environ.get('REDIS_URL', 'localhost:6379')
+#}
+#cache = Cache()
+#cache.init_app(app.server, config=CACHE_CONFIG)
+
+
+## Create a function to convert a given list of tweets into 
+## a Pandas DataFrame. DataFrame should only contain the values we need
+
+results = []
+
+def toDataFrame(tweets):
+
+    DataSet = df()
+
+    return DataSet
+
+##Pass the tweets list to the above function to create a DataFrame
+DataSet = toDataFrame(results)
+
+
+
 
 def generate_table(dataframe, max_rows=5):
     return html.Table(
@@ -80,8 +114,11 @@ def generate_table(dataframe, max_rows=5):
 app.css.append_css(
     {"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
+	
+##here I deleted the value in the value field and put HackUNT; could do
+	
 app.layout = html.Div(children=[
-    dcc.Input(id='my-id', value='Software', type='text'),
+    dcc.Input(id='my-id', value='', type='text'),
     html.Div(id='my-div'),
     
 	html.H2(children='Twitter Sentiment for {{my-id.value}}'),
@@ -93,21 +130,26 @@ app.layout = html.Div(children=[
     generate_table(df_summary_stats),
     html.Div(children='''
         Parsing 1,000 tweets and conducting sentiment analysis with VADER.
-    '''),
+    '''),	
+	
 	
 ])
 
-	
+
+
+##Output targeting the children
+
 @app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)	
-	
+   Output(component_id='my-div', component_property='children'),
+   [Input(component_id='my-id', component_property='value')]
+ 
 	
 ##updating based on data argument from the tweepy-vader data in line 33	
-##added in set and reset indexes
 
-def update_value(df):
+def update_output_div(input_value):
+
+##defining DataSet...
+df_ds = DataSet(input_data, results)
 
     return dcc.Graph(
         id="test",
